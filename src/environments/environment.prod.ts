a01cc7456668d55a4f5d618f0c3338dabfca184e
Lydia Batchelor <lydia.l.batchelor@gmail.com>
2018-07-15 12:15:29 -0500

    routes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCGmLAC1y2HTnbqynCJR9I8bzg1k-gmO_s',
+    authDomain: 'oshop-80cf0.firebaseapp.com',
+    databaseURL: 'https://oshop-80cf0.firebaseio.com',
+    projectId: 'oshop-80cf0',
+    storageBucket: 'oshop-80cf0.appspot.com',
+    messagingSenderId: '334682200340'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCGmLAC1y2HTnbqynCJR9I8bzg1k-gmO_s',
+    authDomain: 'oshop-80cf0.firebaseapp.com',
+    databaseURL: 'https://oshop-80cf0.firebaseio.com',
+    projectId: 'oshop-80cf0',
+    storageBucket: 'oshop-80cf0.appspot.com',
+    messagingSenderId: '334682200340'
+  }
 };
 
 /*
