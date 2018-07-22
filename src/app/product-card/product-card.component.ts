a18bb2b28f283a7b4198f59e904fe9aa1537f72e
Lydia Batchelor <lydia.l.batchelor@gmail.com>
2018-07-21 20:15:02 -0500

    quantity

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,3 +1,4 @@
+import { ShoppingCartService } from './../shopping-cart.service';
 import { Component, Input } from '@angular/core';
 import { Product } from '../models/product';
 
@@ -10,7 +11,23 @@ export class ProductCardComponent {
   @Input('product') product: Product;
   // tslint:disable-next-line:no-input-rename
   @Input('show-actions') showActions = true;
+  // tslint:disable-next-line:no-input-rename
+  @Input('shopping-cart') shoppingCart;
 
-  constructor() { }
+  constructor(private cartService: ShoppingCartService) { }
 
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+  }
+
+  getQuantity() {
+    if (!this.shoppingCart) {
+      return 0;
+    }
+    const item = this.shoppingCart.payload.val().items[this.product.key];
+    if (!item) {
+      return 0;
+    }
+    return item.quantity;
+  }
 }
