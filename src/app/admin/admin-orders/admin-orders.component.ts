0d1d32bc81bc5d1e928b47b3d04378afd8d87df8
Lydia Batchelor <lydia.l.batchelor@gmail.com>
2018-07-28 17:26:45 -0500

    view orders

--- src/app/admin/admin-orders/admin-orders.component.ts
+++ src/app/admin/admin-orders/admin-orders.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Order } from './../../models/order';
+import { OrderService } from './../../order.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-admin-orders',
   templateUrl: './admin-orders.component.html',
   styleUrls: ['./admin-orders.component.css']
 })
-export class AdminOrdersComponent implements OnInit {
+export class AdminOrdersComponent implements OnInit, OnDestroy {
+  displayedColumns: string[] = ['customer', 'date'];
+  orders$;
+  orders;
+  subscription: Subscription;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private orderService: OrderService) {
   }
 
+  ngOnInit() {
+    this.orders$ = this.orderService.getOrders().snapshotChanges();
+    this.subscription = this.orders$.subscribe(
+      o => {
+        this.orders = o;
+      }
+    );
+
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
--- src/app/order.service.ts
+++ src/app/order.service.ts
@@ -13,6 +13,14 @@ export class OrderService {
     const result = await this.db.list('/orders').push(order);
     this.shoppingCartService.clearCart();
     return result;
+  }
 
+  getOrders() {
+    return this.db.list('/orders');
+  }
+
+  getOrdersByUser(userId: string) {
+    return this.db.list('/orders', ref =>
+      ref.orderByChild('userId').equalTo(userId)).snapshotChanges();
   }
 }
